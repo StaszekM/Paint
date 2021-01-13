ebf644cacbd9422fe22b240a05ec21f5be60627c
Stanisław Markowski <stasiomimi@gmail.com>
2021-01-13 22:06:12 +0100

    Make toolbar look better

---
+++ icons/icons.py
@@ -0,0 +1,11 @@
+import os
+
+__directory = os.path.dirname(os.path.abspath(__file__))
+
+colorpicker = os.path.join(__directory, 'colorpicker.png')
+eraser = os.path.join(__directory, 'eraser.png')
+paint = os.path.join(__directory, 'paint.png')
+paint_white = os.path.join(__directory, 'paint_white.png')
+save = os.path.join(__directory, 'save.png')
+width = os.path.join(__directory, 'width.png')
+effect = os.path.join(__directory, 'effect.png')
--- views/toolbar.py
+++ views/toolbar.py
@@ -1,21 +1,33 @@
 from tkinter import *
 from models.paintingOptions import PaintingOptions
+from icons import icons
+import colorsys
 
 
 class Toolbar(Frame):
     def __init__(self, painting_options: PaintingOptions, *args, **kwargs):
-        super().__init__(*args, background='#FF0000', **kwargs)
+        super().__init__(*args, background='#B5F5FF', **kwargs)
         self.painting_options = painting_options
+        self.images = [PhotoImage(file=icon) for icon in
+                       [icons.eraser, icons.paint, icons.width, icons.colorpicker, icons.save, icons.effect,
+                        icons.paint_white]]
 
-        self.eraser_button = Button(self, text='Erase', background='#FFFFFF')
-        self.color_picker_button = Button(self, bg=painting_options.brush_color)
-        self.brush_width_button = Button(self, text=painting_options.brush_width)
-        self.canvas_color_picker_button = Button(self, text='Pick from canvas')
-        self.save_as_button = Button(self, text='Save')
+        self.eraser_button = Button(self, background='#FFFFFF', text='Erase', image=self.images[0], compound=BOTTOM)
+        self.color_picker_button = Button(self, bg=painting_options.brush_color, text='Color', image=self.images[6],
+                                          compound=BOTTOM, fg='#FFFFFF')
+        self.brush_width_button = Button(self, background='#FFFFFF',
+                                         text="Width: {}".format(painting_options.brush_width),
+                                         image=self.images[2], compound=BOTTOM)
 
-        self.blur_button = Button(self, text='Blur')
-        self.color_invert_button = Button(self, text='Invert colors')
-        self.colorize_button = Button(self, text='Colorize')
+        self.canvas_color_picker_button = Button(self, background='#FFFFFF', text='Pick from canvas',
+                                                 image=self.images[3], compound=BOTTOM)
+        self.save_as_button = Button(self, background='#FFFFFF', text='Save', image=self.images[4], compound=BOTTOM)
+
+        self.blur_button = Button(self, background='#FFFFFF', text='Blur', image=self.images[5], compound=BOTTOM)
+        self.color_invert_button = Button(self, background='#FFFFFF', text='Invert colors', image=self.images[5],
+                                          compound=BOTTOM)
+        self.colorize_button = Button(self, background='#FFFFFF', text='Colorize', image=self.images[5],
+                                      compound=BOTTOM)
 
     def setup(self):
         self.eraser_button.grid(row=0, column=0)
@@ -29,8 +41,13 @@ class Toolbar(Frame):
 
     def update_display(self):
         self.eraser_button.configure(bg='#00FF00' if self.painting_options.eraser_enabled else '#FFFFFF')
-        self.color_picker_button.configure(bg=self.painting_options.brush_color)
-        self.brush_width_button.configure(text=self.painting_options.brush_width)
+
+        color = self.painting_options.brush_color[1:]
+        colorRGB = tuple([int(color[i:i + 2], 16) for i in range(0, len(color), 2)])
+        _, _, v = colorsys.rgb_to_hsv(colorRGB[0] / 255, colorRGB[1] / 255, colorRGB[2] / 255)
+        self.color_picker_button.configure(bg=self.painting_options.brush_color, fg='#FFFFFF' if v < 0.6 else '#000000',
+                                           image=self.images[6] if v < 0.6 else self.images[1])
+        self.brush_width_button.configure(text="Width: {}".format(self.painting_options.brush_width))
         self.canvas_color_picker_button.configure(
             bg='#00FF00' if self.painting_options.canvas_color_picker_enabled else '#FFFFFF')
         self.eraser_button.configure(
