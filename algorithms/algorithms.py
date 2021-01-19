8506b55f82a0420fc1629892692777bf6fc6dfe3
Stanisław Markowski <stasiomimi@gmail.com>
2021-01-19 22:21:14 +0100

    Change colorization algorithm a bit

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -24,5 +24,5 @@ class ColorInverter(Processor):
 class Colorizer(Processor):
     def process(self, image: PilImage) -> PilImage:
         white_color = '#%02x%02x%02x' % (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        black_color = '#%02x%02x%02x' % (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        black_color = '#%02x%02x%02x' % (random.randint(0, 128), random.randint(0, 128), random.randint(0, 128))
         return ImageOps.colorize(image.convert("L"), black=black_color, white=white_color)
